0cd6f4a14cba5eb94c57b799b7999d9fd88218ea
Francois Mizrahi <francois.mizrahi@gmail.com>
2025-05-15 16:15:32 +0200

    Change title

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import linkup_api
 
 st.set_page_config(page_title="LinkUp Cruncher", page_icon="🐉​", layout="wide")
-st.title("LinkUp Crunch")
+st.title("Linkup Crunch")
 
 
 with st.sidebar:
